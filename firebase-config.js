19aec94239407ff30c6af6036204dd52cf651cb5
NicklasTSV <nicklas.janzen95@gmail.com>
2025-08-06 10:36:44 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb5J0yQMnNvSOtqfWvqFaaoVBjRRZxUaA",
+    authDomain: "tsv-app-54006.firebaseapp.com",
+    projectId: "tsv-app-54006",
+    storageBucket: "tsv-app-54006.firebasestorage.app",
+    messagingSenderId: "694629601085",
+    appId: "1:694629601085:web:4d40166a2d5445e3113f54",
+    databaseURL: "https://tsv-app-54006-default-rtdb.europe-west1.firebasedatabase.app"
+  };
